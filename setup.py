728d342ff5a71898f90d68d3c3f15c1c8f14924a
Jens Laufer <jenslaufer@gmail.com>
2019-02-19 14:43:19 +0100

    initial

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='scrpproxies',
+    url='https://github.com/jenslaufer/scrpproxies',
+    author='Jens Laufer',
+    author_email='jenslaufer@gmail.com',
+    packages=['scrpproxies'],
+    install_requires=[],
+    version='0.1.0',
+    license='MIT',
+    description='module for proxy rotation for scraping',
+    include_package_data=True
+)
