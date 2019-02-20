db7b90ae7a976b60c6c09def53907ec01d05e037
Jens Laufer <jenslaufer@gmail.com>
2019-02-20 14:31:34 +0100

    changed

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Jens Laufer',
     author_email='jenslaufer@gmail.com',
     packages=['scrpproxies'],
-    install_requires=['pandas', 'numpy'],
+    install_requires=['scipy'],
     version='0.1.0',
     license='MIT',
     description='module for proxy rotation for scraping',
