646db2219255216f60082c6010610fc8026e441e
Jens Laufer <jenslaufer@gmail.com>
2022-07-11 08:20:04 +0200

    Added pandas requirement

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Jens Laufer',
     author_email='jenslaufer@gmail.com',
     packages=['scrpproxies'],
-    install_requires=[],
+    install_requires=['pandas'],
     version='0.2.0',
     license='MIT',
     description='module for proxy rotation for scraping',
